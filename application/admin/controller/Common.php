03ba222314527745060c6664d8f9314eb0340140
原点 <467490186@qq.com>
2018-11-28 16:25:47 +0800

    代码规范

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -8,6 +8,7 @@
 
 namespace app\admin\controller;
 
+use app\admin\model\Config;
 use think\Controller;
 class Common extends Controller
 {
@@ -18,9 +19,9 @@ class Common extends Controller
      */
     protected function initialize()
     {
-        $user = session('user_auth');
-        $this->uid=$user['uid'];
-        $this->group_id=$user['group_id'];
+        $user           = session('user_auth');
+        $this->uid      = $user['uid'];
+        $this->group_id = $user['group_id'];
         $this->config();
         $site_config=$this->siteConfig();
         $this->assign('site_config',$site_config);
@@ -31,13 +32,13 @@ class Common extends Controller
      * @author 原点 <467490186@qq.com>
      */
     private function config(){
-        if(cache('config')){
-            $list=cache('config');
-        }else{
-            $list=db('config')->where('name','=','system_config')->json(['value'])->field('value,status')->find();
+        if ( cache('config')) {
+            $list = cache('config');
+        } else {
+            $list = Config::where('name','=','system_config')->field('value,status')->find();
             cache('config',$list);
         }
-        if($list['status']==1){
+        if( $list['status'] == 1 ){
             config('app_debug',$list['value']['debug']);
             config('app_trace',$list['value']['trace']);
             config('trace.type',$list['value']['trace_type']==0?'Html':'Console');
@@ -46,14 +47,17 @@ class Common extends Controller
 
     /**
      * 站点配置信息
-     * @return array|mixed|null|\PDOStatement|string|\think\Model
+     * @return array|mixed|\PDOStatement|string|\think\Model|null
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
      */
     private function siteConfig(){
-        $site_config=cache('site_config');
-        if($site_config){
+        $site_config = cache('site_config');
+        if ( $site_config ){
             return $site_config;
         }
-        $list=db('config')->where('name','=','site_config')->json(['value'])->field('value')->find();
+        $list = Config::where('name','=','site_config')->field('value')->find();
         cache('site_config',$list);
         return $list;
     }
