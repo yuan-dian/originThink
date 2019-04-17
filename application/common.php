83753d4e3c3ec6506d6c976e220c9a1e5a165199
原点 <467490186@qq.com>
2019-04-17 13:26:37 +0800

    增加公共函数

--- application/common.php
+++ application/common.php
@@ -142,3 +142,22 @@ function alert_error($msg='', $url=null, $time=3){
     throw new think\exception\HttpResponseException($response);
 }
 
+/**
+ * 通用化API接口数据输出
+ * @param int $status 业务状态码
+ * @param string $message 信息提示
+ * @param [] $data  数据
+ * @param int $httpCode http状态码
+ * @return array
+ */
+function show($status, $message, $data=[], $httpCode=200) {
+
+    $data = [
+        'status' => $status,
+        'message' => $message,
+        'data' => $data,
+    ];
+
+    return json($data, $httpCode);
+}
+
