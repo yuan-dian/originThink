9dd5776460f724d51e3babefc992e77f99d3ca04
原点 <467490186@qq.com>
2019-05-22 16:41:45 +0800

    1、增加系统公告 2、增加功能设定 3、修改登录提示时间 4、首页增加文档地址

--- public/js/cache.js
+++ public/js/cache.js
@@ -7,10 +7,10 @@ layui.use(['form','jquery',"layer"],function() {
 
     //判断是否web端打开
     if(!/http(s*):\/\//.test(location.href)){
-        layer.alert("请先将项目部署到 localhost 下再进行访问【建议通过tomcat、webstorm、hb等方式运行，不建议通过iis方式运行】，否则部分数据将无法显示");
+        layer.alert("请先将项目部署到 localhost 下再进行访问【建议通过apache、nginx等方式运行，不建议通过iis方式运行】，否则部分数据将无法显示");
     }else{    //判断是否处于锁屏状态【如果关闭以后则未关闭浏览器之前不再显示】
         if(window.sessionStorage.getItem("lockcms") != "true" && window.sessionStorage.getItem("showNotice") != "true"){
-            // showNotice();
+            showNotice();
         }
     }
 
@@ -32,7 +32,10 @@ layui.use(['form','jquery',"layer"],function() {
             id: 'LAY_layuipro',
             btn: ['火速围观'],
             moveType: 1,
-            content: '<div style="padding:15px 20px; text-align:justify; line-height: 22px; text-indent:2em;border-bottom:1px solid #e2e2e2;"><p class="layui-red">请使用模版前请务必仔细阅读首页右下角的《更新日志》，避免使用中遇到一些简单的问题造成困扰。</p></pclass></p><p>1.0发布以后发现很多朋友将代码上传到各种素材网站，当然这样帮我宣传我谢谢大家，但是有部分朋友上传到素材网站后将下载分值设置的相对较高，需要朋友们充钱才能下载。本人发现后通过和站长、网站管理员联系以后将分值调整为不需要充值才能下载或者直接免费下载。在此郑重提示各位：<span class="layui-red">本模版已进行作品版权证明，不管以何种形式获取的源码，请勿进行出售或者上传到任何素材网站，否则将追究相应的责任。</span></p></div>',
+            content: '<div style="padding:15px 20px; text-align:justify; line-height: 22px; text-indent:2em;border-bottom:1px solid #e2e2e2;">' +
+                '<p class="layui-red">请使用模版前请务必仔细阅读README.md文件，避免使用中遇到一些简单的问题造成困扰。</p>' +
+                '<p class="layui-red">郑重提示：本模版作为学习交流免费使用【不管以何种形式获取的源码，请勿进行出售或者上传到任何素材网站，否则将追究相应的责任】</p>' +
+                '</div>',
             success: function(layero){
                 var btn = layero.find('.layui-layer-btn');
                 btn.css('text-align', 'center');
@@ -70,7 +73,7 @@ layui.use(['form','jquery',"layer"],function() {
     $(".functionSetting").click(function(){
         layer.open({
             title: "功能设定",
-            area: ["380px", "280px"],
+            area: ["380px", "210px"],
             type: "1",
             content :  '<div class="functionSrtting_box">'+
                             '<form class="layui-form">'+
@@ -88,16 +91,9 @@ layui.use(['form','jquery',"layer"],function() {
                                         '<div class="layui-word-aux">开启后切换窗口刷新当前页面</div>'+
                                     '</div>'+
                                 '</div>'+
-                                '<div class="layui-form-item">'+
-                                    '<label class="layui-form-label">单一登陆</label>'+
-                                    '<div class="layui-input-block">'+
-                                        '<input type="checkbox" name="oneLogin" lay-filter="multipleLogin" lay-skin="switch" lay-text="是|否">'+
-                                        '<div class="layui-word-aux">开启后不可同时多个地方登录</div>'+
-                                    '</div>'+
-                                '</div>'+
                                 '<div class="layui-form-item skinBtn">'+
                                     '<a href="javascript:;" class="layui-btn layui-btn-sm layui-btn-normal" lay-submit="" lay-filter="settingSuccess">设定完成</a>'+
-                                    '<a href="javascript:;" class="layui-btn layui-btn-sm layui-btn-primary" lay-submit="" lay-filter="noSetting">朕再想想</a>'+
+                                    '<a href="javascript:;" class="layui-btn layui-btn-sm layui-btn-primary" lay-submit="" lay-filter="noSetting">关闭</a>'+
                                 '</div>'+
                             '</form>'+
                         '</div>',
@@ -119,10 +115,6 @@ layui.use(['form','jquery',"layer"],function() {
                 //取消设定
                 form.on("submit(noSetting)",function(){
                     layer.closeAll("page");
-                });
-                //单一登陆提示
-                form.on('switch(multipleLogin)', function(data){
-                    layer.tips('温馨提示：此功能需要开发配合，所以没有功能演示，敬请谅解', data.othis,{tips: 1})
                 });
                 form.render();  //表单渲染
             }
