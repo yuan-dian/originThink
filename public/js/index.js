a000eb4b634b009340aed295114efd10180f2a47
原点 <467490186@qq.com>
2019-06-03 10:40:25 +0800

    升级版本号，删除无用css、js代码

--- public/js/index.js
+++ public/js/index.js
@@ -11,45 +11,8 @@ layui.use(['bodyTab','form','element','layer','jquery'],function(){
     	layer = parent.layer === undefined ? layui.layer : top.layer;
 		tab = layui.bodyTab({
 			openTabNum : "50",  //最大可打开窗口数量
-			// url : "/json/navs.json" //获取菜单json地址
 		});
 
-	//通过顶部菜单获取左侧二三级菜单   注：此处只做演示之用，实际开发中通过接口传参的方式获取导航数据
-	function getData(json){
-		$.getJSON(tab.tabConfig.url,function(data){
-			if(json == "contentManagement"){
-				dataStr = data.contentManagement;
-				//重新渲染左侧菜单
-				tab.render();
-			}else if(json == "memberCenter"){
-				dataStr = data.memberCenter;
-				//重新渲染左侧菜单
-				tab.render();
-			}else if(json == "systemeSttings"){
-				dataStr = data.systemeSttings;
-				//重新渲染左侧菜单
-				tab.render();
-			}else if(json == "seraphApi"){
-                dataStr = data.seraphApi;
-                //重新渲染左侧菜单
-                tab.render();
-            }
-		})
-	}
-	//页面加载时判断左侧菜单是否显示
-	//通过顶部菜单获取左侧菜单
-	// $(".topLevelMenus li,.mobileTopLevelMenus dd").click(function(){
-	// 	if($(this).parents(".mobileTopLevelMenus").length != "0"){
-	// 		$(".topLevelMenus li").eq($(this).index()).addClass("layui-this").siblings().removeClass("layui-this");
-	// 	}else{
-	// 		$(".mobileTopLevelMenus dd").eq($(this).index()).addClass("layui-this").siblings().removeClass("layui-this");
-	// 	}
-	// 	$(".layui-layout-admin").removeClass("showMenu");
-	// 	$("body").addClass("site-mobile");
-	// 	getData($(this).data("menu"));
-	// 	//渲染顶部窗口
-	// 	tab.tabMove();
-	// })
 
 	//隐藏左侧导航
 	$(".hideMenu").click(function(){
@@ -61,9 +24,6 @@ layui.use(['bodyTab','form','element','layer','jquery'],function(){
 		//渲染顶部窗口
 		tab.tabMove();
 	})
-
-	//通过顶部菜单获取左侧二三级菜单   注：此处只做演示之用，实际开发中通过接口传参的方式获取导航数据
-	// getData("contentManagement");
 
 	//手机设备的简单适配
     $('.site-tree-mobile').on('click', function(){
@@ -153,15 +113,4 @@ function donation(){
             content : "<div style='padding:5px;overflow:hidden;background:#d2d0d0;'><img src='/images/alipay.png'></div>"
         }]
     })
-}
-
-//图片管理弹窗
-function showImg(){
-    $.getJSON('json/images.json', function(json){
-        var res = json;
-        layer.photos({
-            photos: res,
-            anim: 5
-        });
-    });
 }
