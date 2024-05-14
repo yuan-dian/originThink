b50d1c79add05e78c7dc3f6393b8ff66f71c21fc
原点 <467490186@qq.com>
2024-05-14 17:32:48 +0800

    feat：优化path的获取

--- app/Request.php
+++ app/Request.php
@@ -3,14 +3,34 @@
 namespace app;
 
 // 应用请求对象类
+use think\facade\Config;
+
 class Request extends \think\Request
 {
+    /**
+     * 获取当前请求URL的pathinfo信息(不含URL后缀)
+     * @access public
+     * @return string
+     */
     public function path()
     {
-        $dirname = pathinfo($this->pathinfo, PATHINFO_DIRNAME);
-        $basename = pathinfo($this->pathinfo, PATHINFO_FILENAME);
+        if (is_null($this->path)) {
+            $suffix   = Config::get('route.url_html_suffix');
+            $pathinfo = $this->pathinfo();
 
-        return $dirname . '/' . $basename;
+            if (false === $suffix) {
+                // 禁止伪静态访问
+                $this->path = $pathinfo;
+            } elseif ($suffix) {
+                // 去除正常的URL后缀
+                $this->path = preg_replace('/\.(' . ltrim($suffix, '.') . ')$/i', '', $pathinfo);
+            } else {
+                // 允许任何后缀访问
+                $this->path = preg_replace('/\.' . $this->ext() . '$/i', '', $pathinfo);
+            }
+        }
+
+        return $this->path;
     }
 
 }
